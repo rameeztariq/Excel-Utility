a9880f57b8a684642f9c4f5b0e47bed9cb38d793
EVE Virtual Events <RTariq@eve.tech>
2021-09-24 19:43:55 +0400

    read data from excel sheet changes

--- ExcelUtility/Program.cs
+++ ExcelUtility/Program.cs
@@ -21,13 +21,44 @@ namespace ExcelUtility
                 Directory.CreateDirectory(root);
             }
             // Concatenate the file 
-            var filePath = root+@"\Test.xlsx";
+            var filePath = $"{root}{@"\Test.xlsx"}";
             // The whole file path which will be used for saving the data into the excel sheet
             var file = new FileInfo(filePath);
+
+            #region "Save Data into excel"
             // Create a list of users
             var users = CreateData();
             // Finally generate excel sheet
             await GenerateExcelFile(users, file);
+            #endregion
+
+            #region "Read data from excel"
+            //Fetch excel data
+            List<UserModel> usersList = await LoadExcelFile(file);
+            foreach (var user in usersList)
+            {
+                Console.WriteLine($"{user.Id} {user.FirstName} {user.LastName}");
+            }
+            #endregion
+        }
+
+        private static async Task<List<UserModel>> LoadExcelFile(FileInfo file)
+        {
+            List<UserModel> users = new List<UserModel>();
+            using var excelFile = new ExcelPackage(file);
+            var ws = excelFile.Workbook.Worksheets[0];
+            int rows = 2;
+            int cols = 1;
+            while(string.IsNullOrWhiteSpace(ws.Cells[rows,cols].Value?.ToString())==false)
+            {
+                UserModel user = new UserModel();
+                user.Id = Convert.ToInt32(ws.Cells[rows, cols].Value.ToString());
+                user.FirstName =ws.Cells[rows, cols + 1].Value.ToString();
+                user.LastName = ws.Cells[rows, cols + 2].Value.ToString();
+                users.Add(user);
+                rows += 1;
+            }
+            return users;
         }
 
         private static async Task GenerateExcelFile(List<UserModel> users, FileInfo file)
